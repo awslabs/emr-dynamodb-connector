c602e0bbb11b8ea18f05b7d0ae0ca5e9868b438a
sivakumar <srramas@amazon.com>
2017-08-02 15:09:55 +1000

    review changes

--- emr-dynamodb-hadoop/src/main/java/org/apache/hadoop/dynamodb/key/DynamoDBBooleanKey.java
+++ emr-dynamodb-hadoop/src/main/java/org/apache/hadoop/dynamodb/key/DynamoDBBooleanKey.java
@@ -10,6 +10,7 @@
  * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under the License.
  */
+
 package org.apache.hadoop.dynamodb.key;
 
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
