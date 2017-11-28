fd4d241a858c3efb04fbf8bc383332265b7d012a
Yannik Stein <ystein@users.noreply.github.com>
2017-11-28 21:40:42 +0100

    Fix typo in AbstractDynamoDBRecordWriter (#57)

--- emr-dynamodb-hadoop/src/main/java/org/apache/hadoop/dynamodb/write/AbstractDynamoDBRecordWriter.java
+++ emr-dynamodb-hadoop/src/main/java/org/apache/hadoop/dynamodb/write/AbstractDynamoDBRecordWriter.java
@@ -65,7 +65,7 @@ public abstract class AbstractDynamoDBRecordWriter<K, V> implements RecordWriter
   private int batchSize = 0;
   private long intervalBeginTime = 0;
   private long nextPrintCount = PRINT_COUNT_INCREMENT;
-  private long totolItemsWritten = 0;
+  private long totalItemsWritten = 0;
   private double totalIOPSConsumed = 0;
   private long writesPerSecond = 0;
 
@@ -109,7 +109,7 @@ public abstract class AbstractDynamoDBRecordWriter<K, V> implements RecordWriter
         permissibleWritesPerSecond - writesPerSecond, reporter);
 
     batchSize++;
-    totolItemsWritten++;
+    totalItemsWritten++;
 
     if (result != null) {
       if (result.getConsumedCapacity() != null) {
@@ -131,7 +131,7 @@ public abstract class AbstractDynamoDBRecordWriter<K, V> implements RecordWriter
   @Override
   public void close(Reporter reporter) throws IOException {
     client.close();
-    log.info(totolItemsWritten + " total items written");
+    log.info(totalItemsWritten + " total items written");
   }
 
   /**
@@ -149,8 +149,8 @@ public abstract class AbstractDynamoDBRecordWriter<K, V> implements RecordWriter
       }
       permissibleWritesPerSecond = iopsController.getTargetItemsPerSecond();
 
-      if (totolItemsWritten > nextPrintCount) {
-        log.info("Total items written: " + totolItemsWritten);
+      if (totalItemsWritten > nextPrintCount) {
+        log.info("Total items written: " + totalItemsWritten);
         log.info("New writes per second: " + permissibleWritesPerSecond);
         nextPrintCount += PRINT_COUNT_INCREMENT;
       }
