420e2cb1f034368738d1d57fe1f85b64748cab79
Derick Anderson <anderson.derick.w@gmail.com>
2018-03-19 11:10:24 +0900

    Make DynamoDBItemWritable's chunking of serialized strings backwards compatible

--- emr-dynamodb-hadoop/src/main/java/org/apache/hadoop/dynamodb/DynamoDBItemWritable.java
+++ emr-dynamodb-hadoop/src/main/java/org/apache/hadoop/dynamodb/DynamoDBItemWritable.java
@@ -20,6 +20,8 @@ import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
 import org.apache.hadoop.io.Writable;
 
+import java.io.DataInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
@@ -28,7 +30,6 @@ import java.lang.Math;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
-import java.lang.System;
 
 public class DynamoDBItemWritable implements Writable, Serializable {
 
@@ -38,6 +39,8 @@ public class DynamoDBItemWritable implements Writable, Serializable {
   static final String START_OF_TEXT = Character.toString((char) 02);
   // ^C, a.k.a ETX
   static final String END_OF_TEXT = Character.toString((char) 03);
+  private static final char FIRST_MAGIC_BYTES = 0x0001;
+  private static final byte NEXT_MAGIC_BYTE = 0x00;
 
   private Map<String, AttributeValue> dynamoDBItem = new HashMap<>();
 
@@ -47,13 +50,52 @@ public class DynamoDBItemWritable implements Writable, Serializable {
   // format itself and make sure that's backward compatible.
   @Override
   public void readFields(DataInput in) throws IOException {
-    int chunks = in.readInt();
-    String whole = "";
-    for (int c = 0; c < chunks; c++) {
-        String chunk = in.readUTF();
-        whole += chunk;
+    readFieldsStream(readStringFromDataInput(in));
+  }
+
+  // Reads what might be either the result of a single call to
+  // DataOutput.writeUTF() (serialized with a previous version of this class) or
+  // the current chunked format.  DataOutput.writeUTF8 writes a two byte length
+  // (in number of bytes) field, and then writes "modified UTF8" to encode the
+  // string.  Crucially, in that modified UTF8 format the null character
+  // '\u0000' is always written with two bytes.  Thus the sequence 00000000
+  // 00000001 00000000 will not occur as a result of DataOutput.writeUTF and so
+  // we use it as a magic sequence signifying the chunked format.
+  private String readStringFromDataInput(DataInput in) throws IOException {
+    byte[] data;
+    char firstBytes = in.readChar();
+
+    if (firstBytes == FIRST_MAGIC_BYTES) {
+      byte nextByte = in.readByte();
+      if (nextByte == NEXT_MAGIC_BYTE) {
+        // After those three magic bytes the real input begins
+        return readChunks(in);
+      } else {
+        data = new byte[3];
+        data[0] = (byte) (firstBytes >> 8);
+        data[1] = (byte) firstBytes;
+        data[2] = nextByte;
+      }
+    } else {
+      data = new byte[firstBytes + 2];
+      data[0] = (byte) (firstBytes >> 8);
+      data[1] = (byte) firstBytes;
+      in.readFully(data, 2, firstBytes);
     }
-    readFieldsStream(whole);
+
+    // In case the read bytes are not from the new input format, back up and
+    // return the result readUTF would have returned
+    return new DataInputStream(new ByteArrayInputStream(data)).readUTF();
+  }
+
+  private String readChunks(DataInput in) throws IOException {
+    int numChunks = in.readInt();
+    StringBuilder whole = new StringBuilder();
+    for (int c = 0; c < numChunks; c++) {
+      String chunk = in.readUTF();
+      whole.append(chunk);
+    }
+    return whole.toString();
   }
 
   @Override
@@ -62,12 +104,16 @@ public class DynamoDBItemWritable implements Writable, Serializable {
     int chunkSize = 1 << 14;
     int chunks = whole.length() / chunkSize;
     if (whole.length() % chunkSize != 0) {
-        chunks += 1;
+      chunks += 1;
     }
+
+    out.writeChar(FIRST_MAGIC_BYTES);
+    out.write(new byte[]{NEXT_MAGIC_BYTE});
+
     out.writeInt(chunks);
-    for (int c = 0; c < chunks; c++){
-        String chunk = whole.substring(c * chunkSize, Math.min((c + 1) * chunkSize, whole.length()));
-        out.writeUTF(chunk);
+    for (int c = 0; c < chunks; c++) {
+      String chunk = whole.substring(c * chunkSize, Math.min((c + 1) * chunkSize, whole.length()));
+      out.writeUTF(chunk);
     }
   }
 
